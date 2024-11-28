8002f53584b66aafbcf448333a4a902814868005
Bharat Odedara <bharatodedara930@gmail.com>
2024-11-28 15:24:27 +0530

    changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-gray-800 text-slate-600 text-center w-full p-4 h-[5%] sm:h-[8%] absolute bottom-0">
-      <p className='text-xs sm:'>Copyright &copy; {new Date().getFullYear()} Bharat Odedara. All rights reserved.</p>
+      <p className='text-xs sm:text-base'>Copyright &copy; {new Date().getFullYear()} Bharat Odedara. All rights reserved.</p>
     </footer>
   );
 }
