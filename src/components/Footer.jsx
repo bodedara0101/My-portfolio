553836787c9f8bd41747f1133cdf078ae4f2cb00
Bharat Odedara <bharatodedara930@gmail.com>
2024-11-29 07:43:06 +0530

    make a footer item align

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-slate-600 text-center w-full p-4 h-[5%] sm:h-[8%] absolute bottom-0">
+    <footer className="bg-gray-800 text-slate-600 text-center w-full p-4 h-[5%] sm:h-[8%] absolute bottom-0 flex flex-col items-center">
       <p className='text-xs sm:text-base'>Copyright &copy; {new Date().getFullYear()} Bharat Odedara. All rights reserved.</p>
     </footer>
   );
